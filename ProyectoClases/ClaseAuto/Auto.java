84a81b98803092828a4549fa3fcf9b93161e9d03
AndresMorquecho <morquechoisrael@hotmail.com>
2024-09-21 14:27:40 -0500

    Método de Acceso Private | Class Auto

--- ProyectoClases/ClaseAuto/Auto.java
+++ ProyectoClases/ClaseAuto/Auto.java
@@ -2,9 +2,31 @@ package com.krakedev;
 
 public class Auto {
 	
-	public int anio;
-	public String marca;
-	public Double precio;
+	private int anio;
+	private String marca;
+	private Double precio;
+	
+	
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
 	
 	
 	
--- ProyectoClases/ClaseAuto/TestAuto.java
+++ ProyectoClases/ClaseAuto/TestAuto.java
@@ -11,18 +11,18 @@ public class TestAuto {
 		auto1 = new Auto ();
 		auto2 = new Auto();
 		
-		auto1.marca = "Chevrolet";
-		auto1.anio = 2012;
-		auto1.precio = 12500.23;
+		auto1.setMarca("Chevrolet");
+		auto1.setAnio(2012);
+		auto1.setPrecio(12500.23);
 		
 		
-		auto2.marca = "KIA";
-		auto2.anio = 2014;
-		auto2.precio = 14000.53;				
+		auto2.setMarca("KIA");
+		auto2.setAnio(2014);
+		auto2.setPrecio(14000.53);				
 		
 		
-		System.out.println("Datos del auto 1. Marca: " + auto1.marca + " Año: " + auto1.anio + " Precio: " + auto1.precio  );
-		System.out.println("Datos del auto 2. Marca: " + auto2.marca + " Año: " + auto2.anio + " Precio: " + auto2.precio  );
+		System.out.println("Datos del auto 1. Marca: " + auto1.getMarca() + " Año: " + auto1.getAnio() + " Precio: " + auto1.getPrecio()  );
+		System.out.println("Datos del auto 2. Marca: " + auto2.getMarca() + " Año: " + auto2.getAnio() + " Precio: " + auto2.getPrecio()  );
 		
 		
 	}
